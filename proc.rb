77d8fcdeaf23e3c9bc3576a8915cfcc55b2aeb04
YouheiKajiro <te0801068@gmail.com>
2018-12-18 23:36:05 +0900

    Procオブジェクトとクロージャ

--- proc.rb
+++ proc.rb
@@ -5,12 +5,30 @@
 # puts add_proc.call(10)
 # puts add_proc.call(10, 20)
 
-def greeting(arrange_proc)
-  puts 'おはよう'
-  text = arrange_proc.call('こんにちは')
-  puts text
-  puts 'こんばんは'
+# def greeting(arrange_proc)
+#   puts 'おはよう'
+#   text = arrange_proc.call('こんにちは')
+#   puts text
+#   puts 'こんばんは'
+# end
+#
+# repeat_proc = proc { |text| text * 2}
+# greeting(repeat_proc)
+
+def generate_proc(array)
+  counter = 0
+  Proc.new do
+    counter += 10
+    array << counter
+  end
 end
 
-repeat_proc = proc { |text| text * 2}
-greeting(repeat_proc)
+values = []
+sample_proc = generate_proc(values)
+
+sample_proc.call
+print values
+
+sample_proc.call
+print values
+
